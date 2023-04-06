ef6da161e7a6c7bf74ee17bb45e166dcc524b52d
SUDHAR-07 <85281859+SUDHAR-07@users.noreply.github.com>
2023-04-07 00:34:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>HI</h1>
+      <h2>YES its working</h2>
+      <p>keaaaapa</p> */}
+      {/* <TodoWrapper/> */}
+      <TodoWrapper/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/EditTodoForm.js
@@ -0,0 +1,19 @@
+import React,{useState} from 'react'
+
+export const EditTodoForm = ({editTodo,task}) => {
+  const [value, setValue] = useState(task.task);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    // console.log(value);
+    editTodo(value,task.id);
+
+    setValue("");
+  };
+  return (
+    <form className='TodoForm' onSubmit={handleSubmit}>
+      <input type="text" className='todo-input' value={value} placeholder='updatae task?' onChange={(e)=> setValue(e.target.value)}/>
+      <button type='submit' className='todo-btn'>UPDATED TASK</button>
+    </form>
+  )
+}
